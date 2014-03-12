12a9115bdc498e71eb7ce50381c963530224ec91
SungJinYoo <dosm123@gmail.com>
2014-03-12 19:54:18 +0900

    add django app 'utils'

--- hanyang_time_table/hanyang_time_table/settings.py
+++ hanyang_time_table/hanyang_time_table/settings.py
@@ -129,6 +129,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'hanyang_time_table.courses',
+    'hanyang_time_table.utils',
 )
 
 # A sample logging configuration. The only tangible logging
